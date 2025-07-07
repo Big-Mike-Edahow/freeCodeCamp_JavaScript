acd876ed9f0a2d31e1022d260b26fdd78ae6aff2
Big-Mike-Edahow <perigran_falcon@yahoo.com>
2025-07-07 13:09:18 -0600

    Initial commit.

---
+++ Build_a_JavaScript_Trivia_Bot/script.js
@@ -0,0 +1,23 @@
+// script.js
+
+// script.js
+
+console.log("Hello! I'm your coding fun fact guide!")
+
+let botName = "Big Mike"
+let botLocation = "Cleveland Ave"
+let favoriteLanguage = "the Go programming language"
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = favoriteLanguage + " is like C for the 21st Century."
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " was created by engineers at Google"
+console.log(codingFact);
+
+codingFact = favoriteLanguage + "'s mascot is the Go Gopher."
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".")
---
+++ Build_a_Sentence_Maker/script.js
@@ -0,0 +1,24 @@
+// script.js
+
+let adjective = "turquoise";
+let noun = "fish";
+let verb = "sunny";
+let place = "lagoon";
+let adjective2 = "huge";
+let noun2 = "shells";
+
+const firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+// Variable reassignment
+adjective = "furry";
+noun = "hamster";
+verb = "dark";
+place = "shoe";
+adjective2 = "fiery";
+noun2 = "leather";
+
+const secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
